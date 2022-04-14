f9acdc45855f31b0dc06806db5ea2eb982be107e
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-14 08:57:30 +0430

    Unit test

--- src/demo/tests.py
+++ src/demo/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
-# Create your tests here.
+
+class PostTestCase(TestCase):
+    def setUp(self):
+        Post.objects.create(title='This is a new test', valid=True)
+
+    def test_post_title(self):
+        post = Post.objects.create(title='This is a new test', valid=True)
+        self.assertEqual(post.title, 'This is a new test')
