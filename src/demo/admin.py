e731f1b66dd2ead309c8c72c2839c5c222505113
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-14 10:39:41 +0430

    Customize admin page

--- src/demo/admin.py
+++ src/demo/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+
+# a new customized method
+def toggle_valid(modeladmin, request, queryset):
+    if modeladmin.valid:
+        queryset.update(valid=False)
+    else:
+        queryset.update(valid=True)
+
+
+toggle_valid.short_description = 'toggle valid'
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'valid')
+    list_filter = ['valid']
+    search_fields = ['title']
+    list_display = ['title', 'valid', 'combine_title_and_valid']
+
+    def combine_title_and_valid(self, obj):
+        return f"{obj.title.upper()} - {str(obj.valid).lower()}"
+
+
+admin.site.register(Post, PostAdmin)
