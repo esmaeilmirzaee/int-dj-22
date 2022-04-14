9d52b30d16679df74f48445ebe29c286fc2dcae7
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-14 09:57:02 +0430

    Define model

--- src/demo/models.py
+++ src/demo/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class PostManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()
+
+    def valid(self):
+        return self.get_queryset().filter(valid=True)
+
+    def in_valid(self):
+        return self.get_queryset().filter(valid=False)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    valid = models.BooleanField(default=False)
+
+    objects = PostManager()
+
+    def __str__(self):
+        return self.title
+
