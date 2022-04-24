7172282e66d5dd7b8df43a7f9724e54bc279cb0b
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-24 19:47:18 +0430

    Enable premium account

--- src/demo/urls.py
+++ src/demo/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import register_user_view, login_user_view, logout_user_view
+from .views import (
+    register_user_view,
+    login_user_view,
+    logout_user_view,
+    home_view,
+    premium_view
+)
 
 
 app_name = 'demo'
@@ -7,5 +13,7 @@ app_name = 'demo'
 urlpatterns = [
     path('signup/', register_user_view, name='register'),
     path('signin/', login_user_view, name='login'),
-    path('logout/', logout_user_view, name='logout')
+    path('logout/', logout_user_view, name='logout'),
+    path('', home_view, name='home'),
+    path('premium/', premium_view, name='premium')
 ]
--- src/demo/views.py
+++ src/demo/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegisterForm, UserLoginForm
 
 
 def register_user_view(request):
+    next_url = request.GET.get('next')
     form = UserRegisterForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
@@ -15,6 +17,8 @@ def register_user_view(request):
             user.save()
             auth_user = authenticate(username=user.username, password=password)
             login(request, auth_user)
+            if next_url:
+                return redirect(next_url)
             return redirect('/')
     context = {
         'form': form,
@@ -23,6 +27,7 @@ def register_user_view(request):
 
 
 def login_user_view(request):
+    next_url = request.GET.get('next')
     form = UserLoginForm(request.POST or None)
     if request.method == "POST":
         print('login')
@@ -31,6 +36,8 @@ def login_user_view(request):
             password = form.cleaned_data['password']
             auth_user = authenticate(username=username, password=password)
             login(request, auth_user)
+            if next_url:
+                return redirect(next_url)
             return redirect('/')
 
     context = {
@@ -43,3 +50,14 @@ def login_user_view(request):
 def logout_user_view(request):
     logout(request)
     return redirect('/')
+
+
+def home_view(request):
+    return render(request, 'home.html', {})
+
+
+@login_required
+def premium_view(request):
+    # if not request.user.is_authenticated:
+    #     return redirect('demo:login')
+    return render(request, 'premium.html')
