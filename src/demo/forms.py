3257199db45bc49abff5bc3ec4925063ccbc84b5
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-04-24 18:14:48 +0430

    Validate user credentials

--- src/demo/forms.py
+++ src/demo/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.conf import settings
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 
 
 # User = settings.AUTH_USER_MODEL
@@ -18,3 +18,22 @@ class UserRegisterForm(forms.ModelForm):
 class UserLoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField()
+
+    def clean(self, *args, **kwargs):
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            print(user, password, user.check_password(password))
+
+            if not user:
+                raise forms.ValidationError('This user doesn\'t exist.')
+
+            if not user.check_password(password):
+                raise forms.ValidationError('Please check your credentials.')
+
+            if not user.is_active:
+                raise forms.ValidationError('Please active your account.')
+
+        return super(UserLoginForm, self).clean()
